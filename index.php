7a2c5984674fbae2d3aa8e4d341afc3d677dad22
arecastudio <ondiisrail@gmail.com>
2017-09-08 10:14:52 +0900

    updates

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <?php
+ob_start();
 session_start();
 require_once('pages/inc.php');
+$conn=new mysqli(HOST,USER,PASS,DB);
+if ($conn->connect_error) die("Connection failed: " . $conn->connect_error);
 
 if (isset($_POST['submit-login'])) {
 	$user=$_POST['username'];
 	$pass=$_POST['password'];
 	if ($user=='admin' && $pass=='admin') {
-		$_SESSION[username]=$user;
+		$_SESSION['username']=$user;
 	}else{
 		echo "salah";
 	}
@@ -20,15 +23,9 @@ if (isset($_POST['submit-login'])) {
 	<meta charset="utf-8">
 	<meta http-equiv="content-Type" content="text/html; charset=utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-
-	<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 	<link rel="stylesheet" href="libs/style.css" />
 </head>
@@ -53,9 +50,106 @@ if (isset($_POST['submit-login'])) {
 		</div>
 		<?php
 	}else{
-		echo $_SESSION['username'];
-		$_SESSION['username']='';
+		#echo $_SESSION['username'];
+		#$_SESSION['username']='';
+
+
+	?>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">ATK Mgr</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <!--li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Link</a></li-->
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">File<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="?ref=divisi">Master Divisi</a></li>
+            <li><a href="#">Data Umum</a></li>
+            <li><a href="#">Keuangan</a></li>
+            <li><a href="#">Kelembagaan</a></li>
+            <li><a href="#">Keamanan & Ketertiban</a></li>
+            <li><a href="#">Lingkungan Hidup</a></li>
+            <!--li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">One more separated link</a></li-->
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Data Keluarga<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Profil Keluarga</a></li>
+            <li><a href="#">Profil Keluarga</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Potensi Kampung<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="?ref=sumber-daya-alam">Sumber Daya Alam</a></li>
+            <li><a href="#">Profil Keluarga</a></li>
+          </ul>
+        </li>
+      </ul>        
+      <!--form class="navbar-form navbar-left">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form-->
+      <ul class="nav navbar-nav navbar-right">
+        <!--li><a href="#">Link</a></li-->
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Utility <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="?ref=log-out">Log Out</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+	<?php
+
+if (isset($_GET['ref'])) {
+	$ref=$_GET['ref'];
+	switch ($ref) {
+		case 'log-out':
+			$_SESSION['username']='';
+			exit(header('Location: '.base_url()));
+			break;
+		case 'divisi':
+			require_once('pages/master-divisi.php');
+			break;
+		default:
+			# code...
+			break;
+	}
+}
+
 	}
 	?>
 </body>
 </html>
+
+<?php
+$conn->close();
+?>
