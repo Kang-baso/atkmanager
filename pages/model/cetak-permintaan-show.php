27d04aae630ebf49f56cf26a6d0602d0df3c4a20
arecastudio <ondiisrail@gmail.com>
2017-10-02 11:23:13 +0900

    push 2

--- pages/model/cetak-permintaan-show.php
+++ pages/model/cetak-permintaan-show.php
@@ -8,14 +8,43 @@ if ( isset($_POST['tgl1']) && isset($_POST['tgl2']) ) {
 	$tgl2=$_POST['tgl2'];$gsl2=trim($tgl2);
 	if(strlen($tgl1)>0 && strlen($tgl2)>0){
 		$i=0;
-		$sql="SELECT nomor,alasan,status,tgl,nik FROM permintaan ORDER BY tgl ASC";
+		$sql="SELECT nomor,alasan,status,tgl,nik FROM permintaan WHERE (date(tgl) BETWEEN ? AND ?) ORDER BY tgl ASC";
 		$stmt=$conn->prepare($sql);
-		#$stmt->execute();
+		$stmt->bind_param('ss',$tgl1,$tgl2);
 		if($stmt->execute()){
+			echo "
+			<div class=\"table-responsive\">
+			<table class=\"table table-hover table-bordered\">
+			<thead>
+				<tr>
+					<th>#</th>
+					<th>Nomor</th>
+					<th>Keterangan</th>
+					<th>Tgl. Posting</th>
+					<th>Status</th>
+					<th>Kontrol</th>
+				</tr>
+			</thead>
+			<tbody>
+			";
 			$result=$stmt->get_result();
 			while($row=$result->fetch_row()){
-				echo $row[0]."<br/>";
+				$i++;
+				echo "
+				<tr>
+					<td>$i</td>
+					<td>$row[0]</td>
+					<td>$row[1]</td>
+					<td>$row[3]</td>
+					<td>".get_status($row[2])."</td>
+					<td>&nbsp;</td>
+				</tr>
+				";
 			}
+			echo "
+			</tbody>
+			</table>
+			</div>";
 		}
 	}
 }
