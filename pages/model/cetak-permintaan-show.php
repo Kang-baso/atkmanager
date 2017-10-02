acd15678e32f7a105d093efb0988094623a32439
arecastudio <ondiisrail@gmail.com>
2017-10-02 11:55:38 +0900

    ok

--- pages/model/cetak-permintaan-show.php
+++ pages/model/cetak-permintaan-show.php
@@ -8,7 +8,9 @@ if ( isset($_POST['tgl1']) && isset($_POST['tgl2']) ) {
 	$tgl2=$_POST['tgl2'];$gsl2=trim($tgl2);
 	if(strlen($tgl1)>0 && strlen($tgl2)>0){
 		$i=0;
-		$sql="SELECT nomor,alasan,status,tgl,nik FROM permintaan WHERE (date(tgl) BETWEEN ? AND ?) ORDER BY tgl ASC";
+		#$sql="SELECT nomor,alasan,status,tgl,nik FROM permintaan WHERE (date(tgl) BETWEEN ? AND ?) ORDER BY tgl ASC";
+		$sql="SELECT DISTINCT p.nomor,p.alasan,p.status,DATE_FORMAT(p.tgl, '%d %M %Y'),d.nama,p.nik,u.nama,m.nik,m.nama FROM permintaan AS p LEFT OUTER JOIN user AS u ON u.nik=p.nik LEFT OUTER JOIN divisi AS d ON d.id=u.id_divisi LEFT OUTER JOIN user AS m ON m.nik=d.nik_manager WHERE  (p.tgl BETWEEN ? AND ?) ORDER BY p.tgl ASC;";
+
 		$stmt=$conn->prepare($sql);
 		$stmt->bind_param('ss',$tgl1,$tgl2);
 		if($stmt->execute()){
@@ -31,13 +33,28 @@ if ( isset($_POST['tgl1']) && isset($_POST['tgl2']) ) {
 			while($row=$result->fetch_row()){
 				$i++;
 				echo "
-				<tr>
+				<tr valign=\"center\">
 					<td>$i</td>
-					<td>$row[0]</td>
+					<td align=\"center\">$row[0]</td>
 					<td>$row[1]</td>
-					<td>$row[3]</td>
-					<td>".get_status($row[2])."</td>
-					<td>&nbsp;</td>
+					<td align=\"center\">$row[3]</td>
+					<td align=\"center\"><b class=\"label label-primary\">".get_status($row[2])."</b></td>
+					<td align=\"center\" title=\"Cetak\">";
+?>
+                                                 <form method="post" target="_blank" action="<?php echo base_url()."pages/reports/cetak-permintaan.php";?>" >
+                                                         <input type="hidden" name="nomor" value="<?php echo $row[0];?>" />
+                                                         <input type="hidden" name="ket" value="<?php echo $row[1];?>" />
+                                                         <input type="hidden" name="tgl" value="<?php echo $row[3];?>" />
+                                                         <input type="hidden" name="div" value="<?php echo $row[4];?>" />
+                                                         <input type="hidden" name="nik1" value="<?php echo $row[5];?>" />
+                                                         <input type="hidden" name="nama1" value="<?php echo $row[6];?>" />
+                                                         <input type="hidden" name="nik2" value="<?php echo $row[7];?>" />
+                                                         <input type="hidden" name="nama2" value="<?php echo $row[8];?>" />
+                                                         <button type="submit" class="btn btn-info btn-sm"><span class="glyphicon glyphicon-print"></span> Cetak</button>
+                                                 </form>
+
+<?php
+				echo "</td>
 				</tr>
 				";
 			}
